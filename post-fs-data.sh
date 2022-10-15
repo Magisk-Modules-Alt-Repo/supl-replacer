58ce0e8fb3872f560dcd37c4343fd84243f9fd51
D3SOX <nico@d3sox.me>
2022-10-15 18:47:39 +0200

    Fix setting `SUPL_HOST` if it does not exist yet

--- post-fs-data.sh
+++ post-fs-data.sh
@@ -8,7 +8,15 @@ NTP_SERVER=europe.pool.ntp.org
 # For new "Treble enabled" devices
 if [ -f /system/vendor/etc/gps.conf ]; then
   cp /system/vendor/etc/gps.conf "${MODDIR}/system/vendor/etc/gps.conf"
-  sed -i "s/^SUPL_HOST=.*/SUPL_HOST=${SUPL_HOST}/" "${MODDIR}/system/vendor/etc/gps.conf"
+
+  if grep -q "^SUPL_HOST=.*" "${MODDIR}/system/vendor/etc/gps.conf"; then
+    # replace if it's already there
+    sed -i "s/^SUPL_HOST=.*/SUPL_HOST=${SUPL_HOST}/" "${MODDIR}/system/vendor/etc/gps.conf"
+  else
+    # append if it's not there yet
+    echo "SUPL_HOST=${SUPL_HOST}" >> "${MODDIR}/system/vendor/etc/gps.conf"
+  fi
+
   sed -i "s/^NTP_SERVER=.*/NTP_SERVER=${NTP_SERVER}/" "${MODDIR}/system/vendor/etc/gps.conf"
   if [ -f "${MODDIR}/system/vendor/etc/.gitkeep" ]; then
     rm "${MODDIR}/system/vendor/etc/.gitkeep"
@@ -23,7 +31,15 @@ fi
 # For older devices
 if [ -f /system/etc/gps.conf ]; then
   cp /system/etc/gps.conf "${MODDIR}/system/etc/gps.conf"
-  sed -i "s/^SUPL_HOST=.*/SUPL_HOST=${SUPL_HOST}/" "${MODDIR}/system/etc/gps.conf"
+
+  if grep -q "^SUPL_HOST=.*" "${MODDIR}/system/etc/gps.conf"; then
+    # replace if it's already there
+    sed -i "s/^SUPL_HOST=.*/SUPL_HOST=${SUPL_HOST}/" "${MODDIR}/system/etc/gps.conf"
+  else
+    # append if it's not there yet
+    echo "SUPL_HOST=${SUPL_HOST}" >> "${MODDIR}/system/etc/gps.conf"
+  fi
+
   sed -i "s/^NTP_SERVER=.*/NTP_SERVER=${NTP_SERVER}/" "${MODDIR}/system/etc/gps.conf"
   if [ -f "${MODDIR}/system/etc/.gitkeep" ]; then
     rm "${MODDIR}/system/etc/.gitkeep"
